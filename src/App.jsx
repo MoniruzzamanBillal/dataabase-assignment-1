f1f8af93a69069c23c8874ab94c9bf59c1fad785
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-28 17:36:00 +0600

    nav bar added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./Component/NavBar";
+
+function App() {
+  return (
+    <div className="mainCOntainer">
+      {/* nav bar starts  */}
+      <div className="navContainer">
+        <NavBar />
+      </div>
+      {/* nav bar ends */}
+    </div>
+  );
+}
+
+export default App;
